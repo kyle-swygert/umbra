746df44868668707453cedb9f798a5da1e32454b
Kyle Swygert <kyleswygert@gmail.com>
2019-02-23 22:39:15 -0800

    added data types for tree

--- UMBRA/src/app/app.module.ts
+++ UMBRA/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Routes, RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -25,3 +27,79 @@ import { AppRoutingModule } from './app-routing.module';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+export type Container<T> = { value: T };
+
+export class TreeNode<T> {
+
+
+  // for a folder, the value will be empty and the child will not bw null. 
+  // for a file, the value will be populated, and will not have a child. 
+
+
+  private value: Data; 
+  private backward: TreeNode<T>;
+  private forward: TreeNode<T>;
+  private child: TreeNode<T>;
+
+
+  constructor( newValue: Data, newBackward: TreeNode<T>, newForward: TreeNode<T>, newChild: TreeNode<T>) {
+
+    this.value = newValue;
+    this.backward = newBackward;
+    this.forward = newForward;
+    this.child = newChild;    
+
+  }
+
+
+  // methods for the type. 
+ 
+  // setters and getters
+  public set setBackward( newBackward: TreeNode<T>) {
+    this.backward = newBackward;
+  }
+  
+  public get getBackward() : TreeNode<T> {
+    return this.backward;
+  }
+  
+  public set setForward( newForward: TreeNode<T>) {
+    this.forward = newForward;
+  }
+  
+  public get getForward() : TreeNode<T> {
+    return this.forward;
+  }
+  
+  public set setChild( newChild: TreeNode<T>) {
+    this.child = newChild;
+  }
+
+  public get getChild() : TreeNode<T> {
+    return this.child;
+  }
+  
+  public set setValue( newValue: Data) {
+    this.value = newValue;
+  }
+  
+  public get getValue() : Data {
+    return this.value;
+  }
+  
+  
+
+
+}
+
+export type LinkedList<T> = T & { next: LinkedList<T>, previous: LinkedList<T>};
+
+export interface Data {
+
+  type: string, 
+  id: string,
+  title: string;
+  //content: LinkedList<Node>;
+
+}
--- UMBRA/src/app/home/home.page.ts
+++ UMBRA/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+import { RouteReuseStrategy } from '@angular/router';
 import { Component } from '@angular/core';
+import { TreeNode, Data } from '../app.module';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,62 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+}
+
+
+export class TreeBuilder {
+
+
+  public root: TreeNode<Data>;
+
+
+  constructor() {
+
+    // // initializing the data in the root node of the tree. 
+
+    this.root = new TreeNode<Data>(null, null, null, null);
+
+  }
+  
+
+  // methods for the tree. 
+
+  public createNode(newID: string, newType: string, newTitle: string) {
+
+    var testData: Data;// { id = '42',  type = 'folder', title = 'French Toast'  };
+    testData.id = newID;
+    testData.type = newType;
+    testData.title = newTitle;
+
+    //this.root.setValue( testData );
+
+
+  }
+
+
+
+  /**
+   * testTree
+   */
+  public testTree() {
+    // set sum sstuff and see if it works. 
+
+    var testTree = new TreeBuilder();
+
+    var testData: Data;// { id = '42',  type = 'folder', title = 'French Toast'  };
+    testData.id = '42';
+    testData.type = 'Folder';
+    testData.title = 'French Toast';
+
+    var testTreeNode = new TreeNode<Data>(testData, null, null, null);
+
+
+    testTree.root.setValue(testData);
+    //testTree.root.setValue()
+
+
+
+  }
 
 }
