b068faebc7f7a72ec53d29148f21379eab15a45b
Amariah Del Mar <amariahdelmar@gmail.com>
2019-02-23 20:37:02 -0800

    removed "list" section

--- UMBRA/src/app/app-routing.module.ts
+++ UMBRA/src/app/app-routing.module.ts
@@ -11,10 +11,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: './home/home.module#HomePageModule'
   },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
   { path: 'templates', loadChildren: './templates/templates.module#TemplatesPageModule' },
   { path: 'about', loadChildren: './about/about.module#AboutPageModule' },
 ];
